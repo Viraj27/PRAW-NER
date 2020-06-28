d7b5538acda31602c1f781c573a53c807aebece2
Viraj Mehta <viraj.mehta30@gmail.com>
2020-06-28 14:06:01 +0530

    Comment Scraping utility

---
+++ commentScraper.py
@@ -0,0 +1,29 @@
+from fileWriter import FileWriter
+import praw
+
+class CommentScraper( object ):
+
+    def __init__( self ):
+        # Comment scraping can be done in read only mode
+        # Hence, no need to provide username and password to init reddit instance 
+        self.reddit      = praw.Reddit('commentScraper')
+        self.topComments = []
+        self.fileWriter  = FileWriter()
+
+    def scrapeTopCommentsUsingPostID( self, postID ):
+        ''' Scrap all the top comments from a post's ID '''
+        post = self.reddit.submission(id = postID)
+        post.comments.replace_more(limit=0)
+        for top_level_comment in post.comments:
+            self.topComments.append(top_level_comment.body)
+
+    def scrapeTopCommentsUsingPostUrl( self, postUrl ):
+        ''' Scrap all the top comments from a post's URL '''
+        post = self.reddit.submission(url = postUrl)
+        post.comments.replace_more(limit=0)
+        for top_level_comment in post.comments:
+            self.topComments.append(top_level_comment.body)
+
+    def sendCommentsToWriter( self ):
+        ''' Send scraped comments to writer '''
+        self.fileWriter.writeCommentsToFile( self.topComments )
--- fileWriter.py
+++ fileWriter.py
@@ -3,7 +3,7 @@ import os
 class FileWriter( object ):
 
     def __init__( self ):
-        self.dir = 'Enter your directory'
+        self.dir = 'E:/Docx/Reddit Saves/'
 
     def writeListToFile( self, subName, itemList ):
         ''' Write contents of a list to a file '''
@@ -11,3 +11,11 @@ class FileWriter( object ):
         with open (self.filePath, 'w') as f:
             for item in itemList:
                 f.write('{0} \n'.format(item))
+
+    def writeCommentsToFile( self, commentsList ):
+        ''' Write all accumulated contents to a file '''
+        self.filePath = os.path.join(self.dir, 'webGames.txt')
+        with open(self.filePath, 'w') as f:
+            for comment in commentsList:
+                f.write('{0} \n'.format(comment))
+    
